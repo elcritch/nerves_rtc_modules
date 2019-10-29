301d73d202a9eb4651fe937ad8b1d8b1bb600736
Jaremy Creechley <creechley@gmail.com>
2019-10-28 19:19:19 -0600

    initial commits

---
+++ lib/nerves_rtc_modules.ex
@@ -0,0 +1,18 @@
+defmodule NervesRtcModules do
+  @moduledoc """
+  Documentation for NervesRtcModules.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesRtcModules.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_rtc_modules_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesRtcModulesTest do
+  use ExUnit.Case
+  doctest NervesRtcModules
+
+  test "greets the world" do
+    assert NervesRtcModules.hello() == :world
+  end
+end
