cdefb83bfc203119a3978c0cd3c798b042ab23c1
Jaremy Creechley <creechley@gmail.com>
2019-10-28 23:54:09 -0600

    update

--- lib/nerves_rtc_modules/ds1307.ex
+++ lib/nerves_rtc_modules/ds1307.ex
@@ -12,7 +12,7 @@ defmodule NervesRtcModules.RTC.Ds1307 do
   @i2c_bus Application.get_env(:nerves_rtc_modules, :i2c_bus, "i2c-1")
   @i2c_address Application.get_env(:nerves_rtc_modules, :i2c_address, 0x68)
 
-  @i2c_read_cmd <<0x02>>
+  @i2c_read_cmd <<0x00>>
   @i2c_read_len 7
 
   @i2c_write_reg 0x00
@@ -25,12 +25,11 @@ defmodule NervesRtcModules.RTC.Ds1307 do
     with {:ok, bus} <- I2C.open(@i2c_bus) do
 
       with {:ok, time_bytes} <- I2C.write_read(bus, @i2c_address, @i2c_read_cmd, @i2c_read_len),
-           << _clock_control_bit::size(1),
-              second::integer-size(7),
+           << _clock_control_bit::size(1), second::integer-size(7),
               _::size(1), minute::integer-size(7),
               _::size(1), hour::integer-size(7),
-              _::size(2), day_of_month::integer-size(6),
               _day_of_week::integer-size(8),
+              _::size(2), day_of_month::integer-size(6),
               _::size(3), month::integer-size(5),
                 year::integer-size(8) >> = time_bytes,
             :ok <- I2C.close(bus) do
@@ -68,8 +67,8 @@ defmodule NervesRtcModules.RTC.Ds1307 do
                          to_bcd(time.second),
                          to_bcd(time.minute),
                          to_bcd(time.hour),
-                         to_bcd(time.month),
                          to_bcd(day_of_week),
+                         to_bcd(time.day),
                          to_bcd(time.month),
                          to_bcd(time.year - 2000) >>,
         :ok <- I2C.write(bus, @i2c_address, payload),
